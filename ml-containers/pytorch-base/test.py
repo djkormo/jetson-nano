d39557add4c9c8273c523a32a075e80ddff5f4b7
djkormo <djkormo@gmail.com>
2019-11-09 22:35:25 +0100

    correcting output of test.py

--- ml-containers/pytorch-base/test.py
+++ ml-containers/pytorch-base/test.py
@@ -1,11 +1,9 @@
+# based on https://stackoverflow.com/questions/48152674/how-to-check-if-pytorch-is-using-the-gpu
+
 import torch
-# https://stackoverflow.com/questions/48152674/how-to-check-if-pytorch-is-using-the-gpu
-print("",torch.__version__)
-print("",torch.cuda.current_device())
-
-print("",torch.cuda.device(0))
-
-print("",torch.cuda.device_count())
-
-print("",torch.cuda.get_device_name(0))
-print("",torch.cuda.is_available())
+print("torch.__version__",torch.__version__)
+print("torch.cuda.current_device()",torch.cuda.current_device())
+print("torch.cuda.device(0)",torch.cuda.device(0))
+print("torch.cuda.device_count(),torch.cuda.device_count())
+print("torch.cuda.get_device_name(0)",torch.cuda.get_device_name(0))
+print("torch.cuda.is_available()",torch.cuda.is_available())
