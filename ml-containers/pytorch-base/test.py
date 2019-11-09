d143c1fde9463174a3cce778ec78a8d17ef198a5
djkormo <djkormo@gmail.com>
2019-11-09 22:37:27 +0100

    correcting test.py file

--- ml-containers/pytorch-base/test.py
+++ ml-containers/pytorch-base/test.py
@@ -4,6 +4,6 @@ import torch
 print("torch.__version__",torch.__version__)
 print("torch.cuda.current_device()",torch.cuda.current_device())
 print("torch.cuda.device(0)",torch.cuda.device(0))
-print("torch.cuda.device_count(),torch.cuda.device_count())
+print("torch.cuda.device_count()",torch.cuda.device_count())
 print("torch.cuda.get_device_name(0)",torch.cuda.get_device_name(0))
 print("torch.cuda.is_available()",torch.cuda.is_available())
